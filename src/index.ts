3e5a2394acdfba97aabff19859eef7e64f3d77a6
LeeSeokHo <rrgks@naver.com>
2023-02-15 19:36:43 +0900

    github token 환경변수 명 변경

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,14 @@ const WEBHOOK_URL = process.env.WEBHOOK_URL as string;
 
 // 현재 이석호 개인 계정 토큰
 // 개인 토큰이 아닌 팀 토큰으로 변경 필요
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN as string;
+const DEVELOPER_TOKEN = process.env.DEVELOPER_TOKEN as string;
 
 async function main() {
   try {
     const owner = 'the-pool';
     const repos: string[] = ['the-pool-api'];
 
-    const pulls = await getPullRequestsFromRepos(owner, repos, GITHUB_TOKEN);
+    const pulls = await getPullRequestsFromRepos(owner, repos, DEVELOPER_TOKEN);
 
     const pendingPulls = pulls.filter((pull) => {
       return !pull.draft && pull.requested_reviewers.length;
